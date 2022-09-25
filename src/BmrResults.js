c94023e6459aae2941bf20785e33a73609546583
Ryan Hubbuck <rhubbuck@gmail.com>
2022-09-25 13:37:41 -0400

    base bmr formula working

---
+++ src/BmrResults.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BmrResults() {
+  return (
+    <div>BmrResults</div>
+  )
+}
+
+export default BmrResults
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,33 +1,75 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 function Calculator() {
+
+    const [sex, setSex] = useState();
+    const [bmr, setBmr] = useState();
+
+    const ageRef = useRef(null);
+    const weightRef = useRef(null);
+    const feetRef = useRef(null);
+    const inchesRef = useRef(null);
+
+    function convertWeight (lbs) {
+        return 10 * (lbs / 2.205)
+    }
+
+    function convertHeight (inches) {
+        return 6.25 * (inches * 2.54)
+    }
+
+    function convertAge (years) {
+        return 5 * years
+    }
+
+    function getBMR (lbs, inches, years, sex) {
+        if (sex === 'MALE') {
+            return convertWeight(lbs) + convertHeight(inches) - convertAge(years) + 5
+        } else {
+            return convertWeight(lbs) + convertHeight(inches) - convertAge(years) - 161
+        }
+        
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let userAge = ageRef.current.value
+        let userWeight = weightRef.current.value
+        let userFeet = feetRef.current.value
+        let userInches = inchesRef.current.value
+        let userHeight = (parseInt(userFeet * 12)) + parseInt(userInches)
+        console.log(getBMR(userWeight, userHeight, userAge, sex))
+        let userResults = getBMR(userWeight, userHeight, userAge, sex)
+        setBmr(userResults)
+      }
+
   return (
     <div>Calculator
-        <form className='border border-black bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/4'>
+        <form onSubmit={handleSubmit} className='border border-black bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/4'>
             <div className='flex flex-col items-center my-5'>
-                <label for='age' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Age</label>
-                <input type='number' id='age' className='appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'></input>
+                <label htmlFor='age' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Age</label>
+                <input ref={ageRef} required type='number' id='age' className='appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'></input>
             </div>
             <h2 className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-4'>Sex</h2>
-            <fieldset className='flex w-full justify-center mb-5'>
+            <fieldset className='flex w-full justify-center mb-5' onChange={event => setSex(event.target.value)}>
                 <div className='mx-4'>
-                    <input type='radio' name='sex' id='male' value='male'></input>
-                    <label for='male' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Male</label>
+                    <input type='radio' name='sex' id='male' value='MALE'></input>
+                    <label htmlFor='male' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Male</label>
                 </div>
                 <div className='mx-4'>
-                    <input type='radio' name='sex' id='female' value='female'></input>
-                    <label for='female' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Female</label>
+                    <input type='radio' name='sex' id='female' value='FEMALE'></input>
+                    <label htmlFor='female' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Female</label>
                 </div>
             </fieldset>
             <div className='flex flex-col items-center my-5'>
-                <label for='weight' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Weight (US pounds)</label>
-                <input type='number' id='weight' className='appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'></input>
+                <label htmlFor='weight' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Weight (US pounds)</label>
+                <input ref={weightRef} required type='number' id='weight' className='appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'></input>
             </div>
             <div className='my-5'>
-                <label for='height_feet' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Height</label>
+                <label htmlFor='height_feet' className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Height</label>
                 <div className='flex justify-center'>
-                    <input type='number' id='height_feet' className='appearance-none block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 w-1/4 mx-2'></input>
-                    <input type='number' id='height_inches' className='appearance-none block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 w-1/4 mx-2'></input>
+                    <input ref={feetRef} required type='number' id='height_feet' className='appearance-none block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 w-1/4 mx-2'></input>
+                    <input ref={inchesRef} required type='number' id='height_inches' className='appearance-none block bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 w-1/4 mx-2'></input>
                 </div>
             </div>
             <button type='submit' className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-6'>Submit</button>
